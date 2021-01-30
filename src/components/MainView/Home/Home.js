4b02b1615b1cb15ee31e5bb57e219a70573a5884
EugeCos <madskillz@gmx.com>
2021-01-29 19:12:21 -0500

    added features to home page

--- src/components/MainView/Home/Home.js
+++ src/components/MainView/Home/Home.js
@@ -1,25 +1,76 @@
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import * as s from './Home.style'
+import { Parallax } from 'react-parallax'
+import { motion } from 'framer-motion'
+
 
 const Home = () => {
+  const history = useHistory();
+
+  // State
+  const [hoveredIndex, setHoveredIndex] = useState(null)
+
+
+  // Effects
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+
+  const parallaxImages = [
+    {
+      url: 'img/par_houses.png',
+      contentText: 'GALLERY',
+      link: 'gallery'
+    },
+    {
+      url: 'img/par_sunset_cut.png',
+      contentText: 'BLOG',
+      link: 'blog'
+    }
+  ]
+
+
+  const parallaxJSX = parallaxImages.map((item, index) => {
+    return (
+      <Parallax 
+        key={`${index}-${item.link}`}
+        bgImage={item.url}
+        strength={500}
+        renderLayer={() => <s.ParallaxOverlay hovered={index === hoveredIndex} />}          
+      >
+        <s.ParallaxContentWrapper onMouseEnter={() => setHoveredIndex(index)} onMouseLeave={() => setHoveredIndex(null)}>
+          <s.ParallaxContent onClick={() => history.push(item.link)}>{item.contentText}</s.ParallaxContent>
+        </s.ParallaxContentWrapper>
+      </Parallax>
+    )
+  })
+
 
   return (
-    <s.Home>
-      <s.ParallaxWrapper>
-        Parallax        
-      </s.ParallaxWrapper>
+    <motion.nav
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.15 }}
+    >
+      <s.Home>
+        <s.ParallaxWrapper>
+          { parallaxJSX }
+        </s.ParallaxWrapper>
 
-
-      <s.AboutWrapper>
-        <s.Headshot src={'img/headshot.png'}/>
-        <s.AboutHeader>ABOUT</s.AboutHeader>        
-        <s.AboutDescription>
-					Marc is a professional designer, architecture student, and photographer in the Philadelphia area. He started
-					teaching himself photography when he began architecture school, utilizing what he learned in classes to
-					differentiate his photos. Chris often imagines what could be, rather than what is. This is reflected in his
-					surreal and fantastical representation of the city he fell in love with, Philadelphia.
-				</s.AboutDescription>
+        <s.AboutWrapper>
+          <s.Headshot src={'img/headshot.png'}/>
+          <s.AboutHeader>ABOUT</s.AboutHeader>        
+          <s.AboutDescription>
+            Marc is a professional designer, architecture student, and photographer in the Philadelphia area. He started
+            teaching himself photography when he began architecture school, utilizing what he learned in classes to
+            differentiate his photos. Chris often imagines what could be, rather than what is. This is reflected in his
+            surreal and fantastical representation of the city he fell in love with, Philadelphia.
+          </s.AboutDescription>
         </s.AboutWrapper>
-    </s.Home>
+      </s.Home>
+    </motion.nav>
   );
 }
 
--- src/components/MainView/Home/Home.style.js
+++ src/components/MainView/Home/Home.style.js
@@ -1,21 +1,50 @@
 import styled from '@emotion/styled';
 
-export const Home = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
+export const Home = styled.div``
 
 // Parallax section ------------------------------
 export const ParallaxWrapper = styled.div`
-  border: 1px solid green;
+  margin-bottom: 40pt;
+`
+
+export const ParallaxOverlay = styled.div`
+  position: absolute;
+  background: ${p => p.hovered ? 'rgba(0, 0, 0, 0.25)' : 'rgba(0, 0, 0, 0.65)'};
+  left: 0;
+  top: 0;
   width: 100%;
-  margin-bottom: 30pt;
+  height: 100%;
+  transition: .4s ease;
+`
+
+export const ParallaxContentWrapper = styled.div`
+  height: 375pt;
+`
+
+export const ParallaxContent = styled.div`
+  background: rgba(0, 0, 0, 0.75);
+  border-radius: 4.5pt;
+  padding: 5pt 10pt;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 22.5pt;
+  color: #fff;
+  letter-spacing: 6pt;
+  cursor: pointer;
+  transition: .3s ease;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.85);
+    padding: 7.5pt 22.5pt
+  }
 `
 // -----------------------------------------------
 
 // About section ---------------------------------
 export const AboutWrapper = styled.div`
+  margin: 0 auto;
   width: 420pt;
   display: flex;
   flex-direction: column;
@@ -24,8 +53,8 @@ export const AboutWrapper = styled.div`
 
 export const Headshot = styled.img`
   width: 320pt;
-  border-radius: 10px;
-  box-shadow: 0px 0px 10px #84817a;
+  border-radius: 7.5pt;
+  box-shadow: 0px 0px 7.5pt #84817a;
   margin-bottom: 30pt;
 `
 
